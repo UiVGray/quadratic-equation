ec49fe3bfb8a242b290893b1f76271a58a2d57da
UiVGray <gray.777777@gmail.com>
2018-02-28 12:23:37 +0300

    Task 4:feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+let arrofstr = equation.split(' ');
+let a = arrofstr[0];
+let b = arrofstr[3] + arrofstr[4];
+let c = arrofstr[7] + arrofstr[8]; 
+let D = (Math.pow(b , 2)) - (4 * a * c);
+let x1 = Math.round((- b - Math.sqrt(D)) / (2 * a));
+let x2 = Math.round((- b + Math.sqrt(D)) / (2 * a));
+let solution = 0;
+if(x1 < x2) {
+    solution = [x1 , x2]; 
+    }
+else {
+    solution = [x2 , x1];
+    }
+return(solution);
 }
